e9efe438bafab49812ef24a42b1a61f4328be52f
JasonQorbin <Jason.Pieterse@Outlook.com>
2024-05-19 10:39:33 +0200

    Basic express unit test setup
    Move express tests

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ server.get('/', (request, response) => {
     response.send("Hi, from the server!");
 })
 
-server.listen(8000, () => {
+const http = server.listen(8000, () => {
     console.log(`Express server now listening on port ${PORT}`);
 })
+
+//Export the server to make the instance accessible to the test suite.
+module.exports.server = server;
+module.exports.http = http;
---
+++ test/sample.test.js
@@ -0,0 +1,22 @@
+const { server, http } = require("../server/server");
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe("Sample test", () => {
+    const requestObject = chai.request(server);
+
+    it("Test1", (done) => {
+        requestObject
+            .get('/')
+            .end((error, response) => {
+                expect(response.status).to.equal(200);
+                expect(response.text).to.equal("Hi, from the server!");
+            })
+        done();
+    });
+
+    after( done => {http.close(done)});
+})
