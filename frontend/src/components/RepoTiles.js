14508625bc6d0310d2bf59d74678be18b831ad68
JasonQorbin <Jason.Pieterse@Outlook.com>
2024-05-27 23:53:23 +0200

    RepoTiles refactor and test
    - Moved the RepoTiles component to its own file.
    - Added a snapshot tests for it.

---
+++ frontend/src/components/RepoTiles.js
@@ -0,0 +1,34 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faArrowUpRightFromSquare} from "@fortawesome/free-solid-svg-icons";
+
+function RepoTiles(props) {
+    const array = props.repositories;
+    const tiles = array.map((repo) => {
+        const updatedAt = new Date(Date.parse(repo.updated_at));
+        const dateOptions = {
+            year: "numeric",
+            month: "short",
+            day: "numeric"
+        }
+        return (
+            <div key={repo.id} className="repo-tile">
+                <div className="left-tile-pane">
+                    <h3><a className="external-link" href={repo.html_url}>{repo.name}<FontAwesomeIcon icon={faArrowUpRightFromSquare} /></a></h3>
+                    <p><em>{repo.description}</em></p>
+                </div>
+                <div className="right-tile-pane">
+                    <p>Languages: <strong>{repo.language}</strong></p>
+                    <p><em>Last commit: {updatedAt.toLocaleString(undefined, dateOptions)}</em></p>
+                </div>
+            </div>
+        );
+    });
+
+    return (
+        <div>
+            {tiles}
+        </div>
+    );
+}
+
+export default RepoTiles;
--- frontend/src/test/userDetail.test.js
+++ frontend/src/test/userDetail.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import renderer from 'react-test-renderer';
 import UserDetailSideBar from "../components/UserDetailSideBar";
+import RepoTiles from "../components/RepoTiles";
 
 test('Full User detail side bar', () => {
     const tree = renderer
@@ -25,6 +26,39 @@ test('User detail side bar without email, location or twitter', () => {
                 fullName="testFullName"
                 username="testUsername"
                 userURL="testUserURL"/>)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+})
+
+test("Repo tiles", () => {
+    const testData = [
+        {
+            updated_at : "2023-12-04T20:43:40Z",
+            id: 11223344,
+            html_url: "testHTMLurl",
+            name: "testName",
+            description: "Some test description",
+            language: "Haskell",
+        },
+        {
+            updated_at : "2023-09-24T20:32:40Z",
+            id: 66992288,
+            html_url: "testHTMLurl2",
+            name: "testName2",
+            description: "Some other test description",
+            language: "Pascal",
+        },
+        {
+            updated_at : "2021-03-04T16:10:40Z",
+            id: 12454512,
+            html_url: "testHTMLurl3",
+            name: "testName3",
+            description: "Yet another test description",
+            language: "Ruby",
+        }
+    ];
+    const tree = renderer
+        .create(<RepoTiles repositories={testData} />)
         .toJSON();
     expect(tree).toMatchSnapshot();
 })
